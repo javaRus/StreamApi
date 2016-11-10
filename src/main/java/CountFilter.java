903087ef53abaddcd75d91ddbae27423aa26d7d1
Ruslan <ruslan.bur@gmail.com>
2016-11-10 11:21:47 +0200

    Create project

--- src/main/java/CountFilter.java
+++ src/main/java/CountFilter.java
@@ -1,16 +1,16 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class CountFilter {
     public static void main(String[] args) {
         AtomicLong atomicLong = new AtomicLong();
         List<MyInt> myInts = Arrays.asList(new MyInt(1), new MyInt(8), new MyInt(5), new MyInt(2));
         myInts.stream()
-            .map(s ->s.plusRandom())
-            .peek(s -> )
+            .map(s -> s.plusRandom())
+            .peek(s ->System.out.println(s.getMyInt()))
+            .collect(Collectors.toList());
     }
 }
 
@@ -22,8 +22,10 @@ class MyInt {
     }
 
     public MyInt plusRandom() {
-        return new MyInt((int) (Math.random() + myInt));
+        return new MyInt( ((int) (Math.random()*5) + myInt));
     }
 
-
+    public int getMyInt() {
+        return myInt;
+    }
 }
