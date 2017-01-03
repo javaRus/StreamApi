c295a22c85f80bb4fd76edb07b211e806756834b
Ruslan <ruslan.bur@gmail.com>
2017-01-03 12:21:26 +0200

    Refactor class

--- src/main/java/CountFilter.java
+++ src/main/java/CountFilter.java
@@ -1,15 +1,14 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CountFilter {
     public static void main(String[] args) {
-        AtomicLong atomicLong = new AtomicLong();
         List<MyInt> myInts = Arrays.asList(new MyInt(1), new MyInt(8), new MyInt(5), new MyInt(2));
         myInts.stream()
-            .map(s -> s.plusRandom())
-            .peek(s ->System.out.println(s.getMyInt()))
+            .map(MyInt::plusRandom)
+            .peek(s -> System.out.println(s.getMyInt()))
             .collect(Collectors.toList());
     }
 }
@@ -21,11 +20,20 @@ class MyInt {
         this.myInt = myInt;
     }
 
-    public MyInt plusRandom() {
-        return new MyInt( ((int) (Math.random()*5) + myInt));
-    }
-
     public int getMyInt() {
         return myInt;
     }
+
+    public MyInt plusRandom() {
+        return new MyInt(((int) (Math.random() * 5) + myInt));
+    }
+
+    public Stream<MyInt> addToStream(Stream<MyInt> stream, MyInt myInt) {
+        return Stream.concat(stream, Stream.of(myInt));
+    }
+}
+
+//@FunctionInterface
+interface FunctionInterface {
+    int getValue();
 }
