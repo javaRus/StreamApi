9448f375e68ad7fb3189ad15b384b2b274fdfff6
Ruslan <ruslan.bur@gmail.com>
2017-01-12 18:17:42 +0200

    method 2

--- src/main/java/SomeClass.java
+++ src/main/java/SomeClass.java
@@ -5,10 +5,15 @@ public class SomeClass {
 
     public static void main(String[] args) {
         method1();
+        method2();
     }
 
     // this method create in master branch
     public static void method1() {
         System.out.println("Do something");
+    }
+
+    public static void method2() {
+        method1();
     }
 }
